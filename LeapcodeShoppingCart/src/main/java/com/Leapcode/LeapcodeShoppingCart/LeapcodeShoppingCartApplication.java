b5d645cad8fbc239b2c69c9a22f32056894ffdaa
xueru rong <37965833+rachelxueru@users.noreply.github.com>
2021-03-20 21:35:52 -0700

    Add files via upload

---
+++ LeapcodeShoppingCart/src/main/java/com/Leapcode/LeapcodeShoppingCart/LeapcodeShoppingCartApplication.java
@@ -0,0 +1,12 @@
+package com.Leapcode.LeapcodeShoppingCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeapcodeShoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeapcodeShoppingCartApplication.class, args);
+	}
+}
---
+++ LeapcodeShoppingCart/src/main/java/com/Leapcode/LeapcodeShoppingCart/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.Leapcode.LeapcodeShoppingCart.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/someRandomPage")
+    public String home() {
+        return "home";
+    }
+}
